0c643d280f77a39f213a507b9e1f6fc8e542ef8f
Alex <ale632007@gmail.com>
2015-10-07 01:28:10 -0400

    Updated Ray class to use the new changes.

--- src/ray.py
+++ src/ray.py
@@ -10,8 +10,8 @@ class Ray(object):
         ''' Initiated a ray with the start vector and direction vector. '''
         self.start = startVector
         self.dir = dirVector
-        self.distance = vec.magnitude(self.dir)
-        self.dir = vec.normalize(self.dir)
+        self.distance = self.dir.magnitude()
+        self.dir.i_normalize()
         # The end is used when intersections are added so
         # we can know where the ray stops.
         self.end = vec.Vector(3)
@@ -22,26 +22,26 @@ class Ray(object):
 
     def roateUsingMatrix(self, matrix):
         ''' Rotate the ray using a matrix. '''
-        self.start = mat.mulV(matrix, self.start)
-        self.dir = mat.mulV(matrix, self.dir)
-        self.dir = vec.normalize(self.dir)
+        self.start = matrix * self.start
+        self.dir = matrix * self.dir
+        self.dir.i_normalize()
 
     def rotateUsingQuaternion(self, quat1):
         ''' Rotate the ray using a quaternion. '''
-        self.start = quat.mulV(quat1, self.start)
-        self.dir = quat.mulV(quat1, self.dir)
-        self.dir = vec.normalize(self.dir)
+        self.start = quat1 * self.start
+        self.dir = quat1 * self.dir
+        self.dir.i_normalize()
 
     def translate(self, matrix):
         ''' Translate the ray using a matrix. '''
-        self.dir = mat.mulV(matrix, self.dir)
-        self.distance = vec.magnitude(self.dir)
-        self.dir = vec.normalize(self.dir)
+        self.dir = matrix, * self.dir
+        self.distance = self.dir.magnitude()
+        self.dir.i_normalize()
 
     def output(self):
         ''' Show information regarding the ray's behaviour. '''
         print ("Ray:")
         print ("Start:")
-        print (self.start)
+        print (self.start.vector)
         print ("Dir:")
-        print (self.dir)
+        print (self.dir.vector)
