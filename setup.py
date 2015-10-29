2b81f3fa4e57ebb91e8288525b3198bd263e24c9
Alex <ale632007@gmail.com>
2015-10-29 16:53:02 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -16,15 +16,23 @@ setup(
   classifiers = [
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: Unix',
             'Topic :: Scientific/Engineering :: Mathematics',
             'Topic :: Software Development :: Libraries',
             'License :: OSI Approved :: BSD License',
 
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: Implementation',
             ],
 )
