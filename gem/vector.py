bd5d9f99289c319942ad188d8ad43c4e62fee7ba
AfaqQureshi007 <106330573+AfaqQureshi007@users.noreply.github.com>
2023-02-24 07:54:55 +0500

    added testcases
    added testcases and resolve one != issue

--- gem/vector.py
+++ gem/vector.py
@@ -111,7 +111,7 @@ def magnitude(size, vecA):
 def normalize(size, vecA):
     length = magnitude(size, vecA)
     temp = zero_vector(size)
-    if length is not 0:
+    if length != 0:
         for i in sm.range(size):
             temp[i] = vecA[i] / length
     return temp
